dcff692d215ff1c0bed019222cb161d074aa3615
Jordy J. Cevallos-Chavez <jcevall1@asu.edu>
2024-08-07 06:22:11 -0700

    Update 1smash.sh

--- 1smash.sh
+++ 1smash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input file name
-input_file="Jor_extracted_fastq_linksf.txt"
+input_file="Jor_extracted_fastq_links.txt"
 
 # Output file name
 output_file="Jor_output_sketches.txt"
@@ -9,13 +9,17 @@ output_file="Jor_output_sketches.txt"
 # Ensure the input file is in Unix format
 dos2unix "$input_file"
 
-# Start number for sample naming
-start_num=5954473
+# Empty the output file if it exists
+> "$output_file"
 
 # Process each URL in the input file
 while IFS= read -r URL; do
-    SAMPLE="ERR${start_num}"
-    curl -L "${URL}" | zcat | sourmash sketch dna -p scaled=10000,k=21 - -o "${SAMPLE}.sig"
-    echo "${SAMPLE}.sig created from ${URL}" >> "$output_file"
-    start_num=$((start_num + 1))
+    # Extract the base name from the URL
+    base_name=$(basename "${URL}" .fastq.gz)
+    
+    # Sketch the DNA and save the .sig file with the extracted base name
+    curl -L "${URL}" | zcat | sourmash sketch dna -p scaled=10000,k=21 - -o "${base_name}.sig"
+    
+    # Record the creation in the output file
+    echo "${base_name}.sig created from ${URL}" >> "$output_file"
 done < "$input_file"
